8208d41fb25a8f6fe02faea3cba1c716b21f14c7
ST10053561 <vjannatha@gmail.com>
2026-01-19 15:53:17 +0200

    push

--- 3_crew/debate/src/debate/crew.py
+++ 3_crew/debate/src/debate/crew.py
@@ -1,55 +1,51 @@
 from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
-
+from crewai.agents.agent_builder.base_agent import BaseAgent
+from typing import List
 
 @CrewBase
 class Debate():
     """Debate crew"""
 
+    agents = 'config/agents.yaml'
+    tasks = 'config/tasks.yaml'
 
-    agents_config = 'config/agents.yaml'
-    tasks_config = 'config/tasks.yaml'
-
+    
     @agent
     def debater(self) -> Agent:
         return Agent(
-            config=self.agents_config['debater'],
+            config=self.agents_config['debater'], # type: ignore[index]
             verbose=True
         )
 
     @agent
     def judge(self) -> Agent:
         return Agent(
-            config=self.agents_config['judge'],
+            config=self.agents_config['judge'], # type: ignore[index]
             verbose=True
         )
 
+    
     @task
     def propose(self) -> Task:
         return Task(
-            config=self.tasks_config['propose'],
+            config=self.tasks_config['propose'], # type: ignore[index]
         )
 
     @task
-    def oppose(self) -> Task:
+    def reporting_task(self) -> Task:
         return Task(
-            config=self.tasks_config['oppose'],
+            config=self.tasks_config['reporting_task'], # type: ignore[index]
+            output_file='report.md'
         )
-
-    @task
-    def decide(self) -> Task:
-        return Task(
-            config=self.tasks_config['decide'],
-        )
-
 
     @crew
     def crew(self) -> Crew:
         """Creates the Debate crew"""
 
         return Crew(
-            agents=self.agents, # Automatically created by the @agent decorator
-            tasks=self.tasks, # Automatically created by the @task decorator
+            agents=self.agents, 
+            tasks=self.tasks, 
             process=Process.sequential,
             verbose=True,
         )
